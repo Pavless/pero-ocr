f94c0795faf6b68fff037a8c7607cf203f63f463
Pavless <pavlissevcik@gmail.com>
2022-03-16 17:03:59 +0100

    Reworked dataset generation

--- user_scripts/gen_spaced_text_dataset.py
+++ user_scripts/gen_spaced_text_dataset.py
@@ -1,158 +1,127 @@
 import json
-import os
-import time
-import argparse
-from typing import List
-from attr import dataclass
+from pathlib import Path
+from argparse import ArgumentParser
+from typing import Dict, List
+from dataclasses import dataclass, asdict
+import cv2
 
-import torch
-import torch.nn.functional as F
 import numpy as np
+from scipy.special import log_softmax
 import pickle
 
 from pero_ocr.document_ocr.layout import PageLayout
-from pero_ocr.force_alignment import force_align, align_text
-
-## Augmented original function greedy_decode_ctc
-## from pero_ocr/ocr_engine/pytorch_ocr_engine.py
-# scores_probs should be N,C,T, blank is last class
-
-def greedy_decode_ctc(logits, characters, keep_blank=True):
-    if len(logits.shape) == 2:
-        logits = np.concatenate((logits[:, 0:1], logits), axis=1)
-        logits[:, 0] = -1000
-        logits[-1, 1] = 1000
-    else:
-        logits = np.concatenate((logits[:, :, 0:1], logits), axis=2)
-        logits[:, :, 0] = -1000
-        logits[:, -1, 0] = 1000
-
-    best = np.argmax(logits, 1) + 1
-    mask = best[:, :-1] == best[:, 1:]
-    best = best[:, 1:]
-    best[mask] = 0
-    best[best == logits.shape[1]] = 0
-    best = best - 1
-    outputs = []
-    for line in best:
-        if keep_blank:
-            outputs.append(''.join([characters[c] if c >= 0 else "~" for c in line]))
-        else:
-            outputs.append(''.join([characters[c] if c >= 0 else "" for c in line]))
-
-    return outputs
-
-def encode_transcription(text, char_to_idx):
-    return [char_to_idx[c]+1 for c in text]
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-x", "--input-xml-path", required=True)
-    parser.add_argument("-o", "--ocr", required=True)
-    parser.add_argument("-l", "--input-logit-path", required=True)
-    parser.add_argument("-d", "--output-dataset-path", default=None)
-    args = parser.parse_args()
-    return args
+from pero_ocr.force_alignment import align_text
+from pero_ocr.document_ocr.crop_engine import EngineLineCropper
+from pero_ocr.ocr_engine.pytorch_ocr_engine import PytorchEngineLineOCR
 
 @dataclass
 class DatasetEntry:
     id: str
-    best_ctc_path: str
+    transcription: str
+    char_positions: List[int]
+
+class LabelEncoder:
+    def __init__(self, char_to_idx: Dict[str, int]):
+        self.char_to_idx = char_to_idx
+
+    def encode(self, label: str):
+        return np.array([self.char_to_idx[c] for c in label])
 
 class DatasetBuilder:
 
-    def __init__(self, input_xml_path, input_logit_path, output_dataset_path, char_to_idx, idx_to_char):
-        self.input_xml_path = input_xml_path
-        self.input_logit_path = input_logit_path
-        self.output_dataset_path = output_dataset_path
-        self.char_to_idx = char_to_idx
-        self.idx_to_char = idx_to_char
+    def __init__(self,
+                 xml_path: Path,
+                 image_path: Path,
+                 dataset_path: Path,
+                 ocr_engine: PytorchEngineLineOCR,
+                 line_cropper: EngineLineCropper,
+                 label_encoder: LabelEncoder,
+                 verbose=False):
+        self.xml_path = xml_path
+        self.image_path = image_path
+        self.dataset_path = dataset_path
 
-        self.paths: List[DatasetEntry] = []
-        self.characters = None
+        self.ocr_engine = ocr_engine
+        self.line_cropper = line_cropper
+        self.label_encoder = label_encoder
 
-    def process_page(self, file_id, index, ids_count):
-        print(f"Processing {file_id}")
-        t1 = time.time()
-        page_layout = PageLayout(file=os.path.join(self.input_xml_path, file_id + '.xml'))
-        page_layout.load_logits(os.path.join(self.input_logit_path, file_id + '.logits'))
+        self.verbose = verbose
 
-        for idx_r, region in enumerate(page_layout.regions):
-            for idx_l, line in enumerate(region.lines):
-                print("@@@@@")
-                print(line.transcription)
-                self.characters = line.characters
-                logits = torch.tensor(line.logits.toarray())
-                logits = logits.unsqueeze(0)
-                logits = logits.permute(0, 2, 1)
-                log_probs = F.log_softmax(logits, dim=1)
+        self.dataset: List[DatasetEntry] = []
 
-                log_probs = torch.squeeze(log_probs).numpy().T
-                best_ctc_path = force_align(-log_probs, encode_transcription(line.transcription, self.char_to_idx), 511)
+    def process_page(self, page_id: str):
+        image = cv2.imread(str(self.image_path / f"{page_id}.jpg"))
+        page_layout = PageLayout(file=str(self.xml_path / f"{page_id}.xml"))
 
-                best_ctc_path = np.array(best_ctc_path) - 1
-                # best_ctc_path = best_ctc_path[np.nonzero(best_ctc_path >= 0)]
-                print(''.join([self.idx_to_char[c] if c >= 0 else "~" for c in best_ctc_path]))
-                # exit()
+        for line in page_layout.lines_iterator():
+            if line.transcription is None:
+                if self.verbose:
+                    print(f"skipping: {page_id}-{line.id}")
+                continue
+            line_crop = self.line_cropper.crop(image, line.baseline, line.heights)
+            line_batched = line_crop[np.newaxis]
 
-                # print(align_text(-log_probs, np.asarray(encode_transcription(line.transcription, self.char_to_idx)) , 0))
+            _, logits, _ = self.ocr_engine.process_lines(line_batched, sparse_logits=False, tight_crop_logits=True)
+            logits = logits[0]
+            log_probs = log_softmax(logits, axis=1)
+            labels = self.label_encoder.encode(line.transcription)
+            char_positions = align_text(-log_probs, labels, log_probs.shape[1] - 1)
+            char_positions = char_positions * self.ocr_engine.net_subsampling
 
-                best_ctc_path = greedy_decode_ctc(np.expand_dims(line.logits.toarray().T, 0), line.characters)
-                best_ctc_path = np.squeeze(best_ctc_path)
-                print(best_ctc_path)
+            dataset_entry = DatasetEntry(f"{page_id}-{line.id}",
+                                         line.transcription,
+                                         [i for i in char_positions])
+            self.dataset.append(dataset_entry)
 
-                best_ctc_path = greedy_decode_ctc(np.expand_dims(line.logits.toarray().T, 0), line.characters, keep_blank=False)
-                best_ctc_path = np.squeeze(best_ctc_path)
-                print(best_ctc_path)
-                best_ctc_path = str(best_ctc_path)
+            if self.verbose:
+                for i, p in enumerate(char_positions):
+                    color = [0,0,255] if line.transcription[i] != " " else [0,255,0]
+                    line_crop[:, p, :] = color
+                cv2.imshow("Character positions", line_crop)
+                cv2.waitKey(0)
 
-                best_ctc_path = force_align(-log_probs, encode_transcription(best_ctc_path, self.char_to_idx), 0)
-
-                best_ctc_path = np.array(best_ctc_path) - 1
-                # best_ctc_path = best_ctc_path[np.nonzero(best_ctc_path >= 0)]
-                print(''.join([self.idx_to_char[c] if c >= 0 else "~" for c in best_ctc_path]))
-                
-                self.paths.append(DatasetEntry(f"{file_id}-{line.id}", best_ctc_path))
-            exit()
-        print("DONE {current}/{total} ({percentage:.2f} %) [id: {file_id}] Time:{time:.2f}".format(
-            current=index + 1, total=ids_count, percentage=(index + 1) / ids_count * 100,
-            file_id=file_id, time=time.time() - t1))
 
     def save(self):
-        paths = {}
-        for entry in self.paths:
-            paths[entry.id] = entry.best_ctc_path
+        dataset = [asdict(item) for item in self.dataset]
+        print(f"Saving, dataset size: {len(dataset)}...")
 
-        dataset = {
-            "characters": self.characters,
-            "paths": paths
-        }
-
-        if self.output_dataset_path is not None:
-            with open(self.output_dataset_path, "wb") as f:
+        if self.dataset_path is not None:
+            with open(self.dataset_path, "wb") as f:
                 pickle.dump(dataset, f)
 
 def main():
-    args = parse_arguments()
+    parser = ArgumentParser()
+    parser.add_argument("-o", "--ocr", help="The OCR .json config file", type=Path, required=True)
+    parser.add_argument("-x", "--xml-path", help="The path to the directory containing .xml page files", type=Path, required=True)
+    parser.add_argument("-i", "--image-path", help="The path to the directory containing page images", type=Path, required=True)
+    parser.add_argument("-d", "--dataset-path", help="The path to the dataset output file", type=Path, default=None)
+    parser.add_argument("-v", "--verbose", help="If specified, the individual character positions for each line are displayed", action="store_true")
+    args = parser.parse_args()
 
-    input_xml_path = args.input_xml_path
-    input_logit_path = args.input_logit_path
-    output_dataset_path = args.output_dataset_path
 
     with open(args.ocr, "r") as f:
         ocr_config = json.load(f)
 
     char_to_idx = {c: i for i, c in enumerate(ocr_config["characters"])}
 
-    dataset_builder = DatasetBuilder(input_xml_path, input_logit_path, output_dataset_path, char_to_idx, ocr_config["characters"])
-
-    logits_to_process = [f for f in os.listdir(input_logit_path)
-                         if os.path.splitext(f)[1] ==  ".logits"]
-    ids_to_process = [os.path.splitext(os.path.basename(file))[0] for file in logits_to_process]
-
-    for index, file_id in enumerate(ids_to_process):
-        dataset_builder.process_page(file_id, index, len(ids_to_process))
+    ocr_engine = PytorchEngineLineOCR(args.ocr, gpu_id=0)
+    line_cropper = EngineLineCropper(line_height=ocr_config["line_px_height"], poly=2, scale=ocr_config["line_vertical_scale"])
+    label_encoder = LabelEncoder(char_to_idx)
+    dataset_builder = DatasetBuilder(args.xml_path,
+                                     args.image_path,
+                                     args.dataset_path,
+                                     ocr_engine,
+                                     line_cropper,
+                                     label_encoder,
+                                     verbose=args.verbose)
     
+    page_ids = [f.stem for f in args.xml_path.glob("*.xml")]
+    total_count = len(page_ids)
+    for i, page_id in enumerate(page_ids):
+        dataset_builder.process_page(page_id)
+        print("                                                         \r", end="")
+        print(f"Progress: {i+1}/{total_count} ({(i+1)/total_count*100:.2f} %)", end="")
+    print()
     dataset_builder.save()
 
 if __name__ == "__main__":
